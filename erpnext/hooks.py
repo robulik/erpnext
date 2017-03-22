625f3d8f1ea86025c54e406d26245690301a1b22
Robert Kirschner <robertkirschner@gmail.com>
2017-03-22 10:37:32 +0100

    Default email footer customization

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -183,8 +183,8 @@ scheduler_events = {
 }
 
 default_mail_footer = """<div style="text-align: center;">
-	<a href="https://erpnext.com?source=via_email_footer" target="_blank" style="color: #8d99a6;">
-		Sent via ERPNext
+	<a href="https://www.sezam.eu" target="_blank" style="color: #8d99a6;">
+		SEZAMiS
 	</a>
 </div>"""
 
